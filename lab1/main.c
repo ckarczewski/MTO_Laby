6ee1b787a5ca47aa5f69051ee8c056a1b3970da3
Czarek Karczewski <keracz1997@gmail.com>
2022-10-02 14:42:42 +0200

    add all chaged file

--- lab1/main.c
+++ lab1/main.c
@@ -2,11 +2,24 @@
 #include <string.h>
 #include <ctype.h>
 
+void change (char *param){
+	for(int j=0; j<strlen(param); j++){
+				char letter = param[j];
+				int letter_number = (int) letter;
+				if (letter_number >=65 && letter_number <= 90) {
+					letter_number = letter_number + 32;
+				} else if (letter_number >=97 && letter_number <=122){
+					letter_number = letter_number - 32;
+				}
+				putchar(letter_number);
+			}
+}
+
 int my_printf(char *format_string, char *param){
 	for(int i=0;i<strlen(format_string);i++){
 		if((format_string[i] == '#') && (format_string[i+1] == 'k')){
 			i++;
-			printf("%s",param);
+			change(param);
 		}else
 			putchar(format_string[i]);
 	}
