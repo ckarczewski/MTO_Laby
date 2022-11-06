dc5ccc2784edfb0d05a21e6e39a69041177e6813
Czarek Karczewski <keracz1997@gmail.com>
2022-11-06 13:09:41 +0100

    add working algorthm

--- lab4/main.py
+++ lab4/main.py
@@ -8,7 +8,11 @@ def my_printf(format_string,param):
     for idx in range(0,len(format_string)):
         if shouldDo:
             if format_string[idx] == '#' and format_string[idx+1] == 'k':
-                print(param,end="")
+                if param.isdigit():
+                    print(param[::-1],end="")
+                else:
+                    print("",end="")
+
                 shouldDo=False
             else:
                 print(format_string[idx],end="")
