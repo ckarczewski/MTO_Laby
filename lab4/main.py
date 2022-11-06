098c04359b54c87b1b4b32c8967c5d906ac37a94
Czarek Karczewski <keracz1997@gmail.com>
2022-11-06 14:35:51 +0100

    add case when first sign of param is 0

--- lab4/main.py
+++ lab4/main.py
@@ -8,7 +8,9 @@ def my_printf(format_string,param):
     for idx in range(0,len(format_string)):
         if shouldDo:
             if format_string[idx] == '#' and format_string[idx+1] == 'g':
-                if param.isdigit():
+                if param[0]=="0" or param[1]=="0":
+                    print("",end="")
+                elif param.isdigit():
                     print(param[::-1],end="")
                 elif param[0]=="-" and param[1:].isdigit():
                     print(param[0] + param[1:][::-1],end="")
