ad708836a86220520c1ae97d86365f6d9f23f341
Czarek Karczewski <keracz1997@gmail.com>
2022-11-20 16:15:25 +0100

    put existing code into decrease_numbers function

--- lab5/main.py
+++ lab5/main.py
@@ -1,36 +1,54 @@
 #!/usr/bin/env python3
 
 import sys
+
 def is_digit(n):
     try:
         int(n)
         return True
     except ValueError:
         return  False
-        
+
+def decrease_numbers(param):
+    number = int(param)
+    new_param = ""
+    if number >= 0:
+        for par in param:
+            if par == "0":
+                temp = "9"
+            else:
+                res = int(par) - 1
+                temp = str(res)
+            new_param = new_param + temp
+            temp = ""
+    elif number < 0:
+        for par in param[1:]:
+            if par == "0":
+                temp = "9"
+            else:
+                res = int(par) - 1
+                temp = str(res)
+            new_param = new_param + temp
+            temp = ""
+    return new_param
+
 def my_printf(format_string,param):
     #print(format_string)
-    t = ""
     shouldDo=True
+    changed_param = ""
     for idx in range(0,len(format_string)):
         if shouldDo:
             if format_string[idx] == '#' and format_string[idx+1] == 'g':
-                if param.isdigit():
-                    for par in param:
-                        if par == "0":
-                            temp = "9"
-                        else:
-                            res = int(par) - 1
-                            temp = str(res)
-                        t = t + temp
-                        temp = ""
-                print(t,end="")
+                if is_digit(param):
+                    changed_param = decrease_numbers(param)
+                print(changed_param,end="")
                 shouldDo=False
             else:
                 print(format_string[idx],end="")
         else:
             shouldDo=True
     print("")
+
 
 data=sys.stdin.readlines()
 
