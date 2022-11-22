cc5f7934efe1134320d7bd59c20d369742eb3c82
Czarek Karczewski <keracz1997@gmail.com>
2022-11-22 20:41:08 +0100

    add x checker function

--- lab5/main.py
+++ lab5/main.py
@@ -34,30 +34,51 @@ def decrease_numbers(param):
             temp = ""
     return new_param
 
+def x_checker(inside, param):
+    if not is_digit(inside): # when inside is not number
+        return ""
+    elif is_digit(inside) and inside.isdigit(): #when inside is (5; 05;)
+        x = int(inside)
+        if inside[0] == "0":
+            return x*"0"+decrease_numbers(param)
+        else:
+            return x*" "+decrease_numbers(param)
+    elif is_digit(inside) and not inside.isdigit(): #when inside is(-5; -05;)
+        x = int(inside)*(-1)
+        if inside[1] == "0":
+            return decrease_numbers(param)+x*"0"
+        else: 
+            return decrease_numbers(param)+x*" " 
+
+            # sprawdzić czy pierwsze jest zerem jeśli tak to niech sprawdzi czy resta jest liczba  
+
+
 def my_printf(format_string,param):
     #print(format_string)
-    shouldDo=True
-    changed_param = ""
-    result = ""
-    for idx in range(0,len(format_string)):
-        if shouldDo:
-            if format_string[idx] == '#':
-                for jdx in range(idx,len(format_string)):
-                    if format_string[jdx] == 'g':
-                        result = re.search('#(.*)g', format_string)
-                        t = result.group(1)
-                        if t.isdigit():
-                            if is_digit(param):
-                                changed_param = decrease_numbers(param)
-                            print(changed_param,end="")
-                            shouldDo=False
-                        else: 
-                            print(format_string[idx],end="")            
+    if not is_digit(param):
+        print("Error", end="")
+        print("")
+    else:
+        shouldDo=True
+        changed_param = ""
+        result = ""
+        for idx in range(0,len(format_string)):
+            if shouldDo:
+                if format_string[idx] == '#':
+                    for jdx in range(idx,len(format_string)):
+                        if format_string[jdx] == 'g':
+                            result = re.search('#(.*)g', format_string)
+                            t = result.group(1)
+                            output = x_checker(t, param)
+                            if output == "":
+                                break
+                            else:
+                                print(output)      
+                else:
+                    print(format_string[idx],end="")
             else:
-                print(format_string[idx],end="")
-        else:
-            shouldDo=True
-    print("")
+                shouldDo=True
+        print("")
 
 
 data=sys.stdin.readlines()
