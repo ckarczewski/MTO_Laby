3a9be9265332652d3b198a0b94c373ad6a8e98d4
Czarek Karczewski <keracz1997@gmail.com>
2022-11-24 18:30:43 +0100

    redone x checker function, redone algorithm logic

--- lab5/main.py
+++ lab5/main.py
@@ -34,51 +34,47 @@ def decrease_numbers(param):
             temp = ""
     return new_param
 
-def x_checker(inside, param):
-    if not is_digit(inside): # when inside is not number
-        return ""
-    elif is_digit(inside) and inside.isdigit(): #when inside is (5; 05;)
-        x = int(inside)
-        if inside[0] == "0":
+def x_checker(number, whole, param):
+    try:
+        whole.index("-")
+        sign = True
+    except:
+        sign = False
+
+    if sign == True:
+        x = int(number)
+        if number[0] == "0":
+            return decrease_numbers(param)+x*"0"
+        else: 
+            return decrease_numbers(param)+x*" "
+    else:
+        x = int(number)
+        if number[0] == "0":
             return x*"0"+decrease_numbers(param)
         else:
             return x*" "+decrease_numbers(param)
-    elif is_digit(inside) and not inside.isdigit(): #when inside is(-5; -05;)
-        x = int(inside)*(-1)
-        if inside[1] == "0":
-            return decrease_numbers(param)+x*"0"
-        else: 
-            return decrease_numbers(param)+x*" " 
-
             # sprawdzić czy pierwsze jest zerem jeśli tak to niech sprawdzi czy resta jest liczba  
 
 
 def my_printf(format_string,param):
     #print(format_string)
+    x_param=""
+    pattern = re.compile(r'#-?(\d+)g')
     if not is_digit(param):
         print("Error", end="")
         print("")
     else:
-        shouldDo=True
-        changed_param = ""
-        result = ""
-        for idx in range(0,len(format_string)):
-            if shouldDo:
-                if format_string[idx] == '#':
-                    for jdx in range(idx,len(format_string)):
-                        if format_string[jdx] == 'g':
-                            result = re.search('#(.*)g', format_string)
-                            t = result.group(1)
-                            output = x_checker(t, param)
-                            if output == "":
-                                break
-                            else:
-                                print(output)      
-                else:
-                    print(format_string[idx],end="")
-            else:
-                shouldDo=True
-        print("")
+        if ((pattern.search(format_string)) != None):
+            result = pattern.search(format_string)
+            number = result.group(1)
+            whole = result.group(0)
+            out = x_checker(number, whole, param)
+            new_string = format_string.replace(whole, out)
+            print(new_string, end="")
+            print("")
+        else:
+            print(format_string, end="")
+            print("")
 
 
 data=sys.stdin.readlines()
